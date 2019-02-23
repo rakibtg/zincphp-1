d76bbce3d0833538e249cea08d3dea623bfaf0a4
rakibtg <rakibtg@gmail.com>
2019-02-24 01:38:10 +0600

    examples with middleware payload data

--- middlewares/Middlewares.php
+++ middlewares/Middlewares.php
@@ -4,14 +4,20 @@
   class Middlewares extends \ZincPHP\Middleware\ZincMiddleware {
 
     public function blocks() {
-      $this->setBlock('index/post', 'testMiddleware');
-      $this->setBlock('index/post', ['testMiddleware', 'tommatto']);
+      // $this->setBlock('index', 'testMiddleware');
+      // $this->setBlock('index', ['tommatto' => [
+      //   'name' => 'Kazi',
+      //   'age' => '23'
+      // ]]);
+      // $this->setBlock('index/post', 'testMiddleware');
+      // $this->setBlock('index', ['testMiddleware', 'tommatto']);
       // $this->setBlock('index/potato', 'potato');
     }
 
     public function groups() {
-      $this->setGroup('index', ['tommatto', 'testMiddleware']);
-      $this->setGroup('index', 'testMiddleware');
+      // $this->setGroup('index', ['tommatto' => ['yes']]);
+      // $this->setGroup('index', ['protected_photos' => 'yes']);
+      // $this->setGroup('index', 'testMiddleware');
     }
 
   }
