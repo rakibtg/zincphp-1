1ee3451da137778e5153d772f41aaabc42557901
rakibtg <rakibtg@gmail.com>
2019-02-25 20:01:14 +0600

    writing tests

---
+++ tests/UnitTestFiles/Test/ZincBlockTest.php
@@ -0,0 +1,5 @@
+<?php
+
+  class ZincBlockTest extends ZincPHPTests {
+    
+  }
---
+++ tests/ZincPHPTests.php
@@ -0,0 +1,10 @@
+<?php
+
+  namespace ZincPHP\Tests;
+
+  define('ZINCPHP_ROOT', realpath(__DIR__ . '/../'));
+  require_once '../vendor/autoload.php';
+
+  class ZincPHPTests extends PHPUnit\Framework\TestCase\TestCase {
+    
+  }
